e51a9a910f870e80132c69ea86025025c819d703
MonikaDabhade <monikadabhade1998@gmail.com>
2023-01-13 22:56:49 +0530

    ybhuihsuidnaj

--- src/p1/C1.java
+++ src/p1/C1.java
@@ -6,7 +6,7 @@ public class C1
 	public static void main(String[] args)
 	{
 		System.out.println("this is class C1");
-<<<<<<< Updated upstream
+
 		System.out.println("stash3");
 
 		
@@ -14,11 +14,10 @@ public class C1
 		
 		
 		
-		
-=======
+
 		
 		System.out.println("stash1****************");
->>>>>>> Stashed changes
+
 	}
 
 }
