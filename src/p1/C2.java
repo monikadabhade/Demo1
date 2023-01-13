67124b1c0d5c6177e6c87c298137091443243cda
MonikaDabhade <monikadabhade1998@gmail.com>
2023-01-13 12:59:26 +0530

    njlhub

--- src/p1/C2.java
+++ src/p1/C2.java
@@ -1,9 +1,11 @@
 package p1;
 
-public class C2 {
+public class C2 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		
 
 	}
 
